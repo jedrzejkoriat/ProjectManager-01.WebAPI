dee80c669e3672f3af95bf885dbd5b8691d2a936
jedrzejkoriat <jedrzej.koriat@gmail.com>
2025-05-16 01:17:59 +0200

    Feat: add ticket types controller

---
+++ Controllers/TicketTypesController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProjectManager_01.WebAPI.Data;
+
+namespace ProjectManager_01.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketTypesController : ControllerBase
+    {
+        private static List<TicketType> ticketTypes = new List<TicketType>
+        {
+            new TicketType { Id = 1, Name = "Bug" },
+            new TicketType { Id = 2, Name = "Task" }
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<TicketType>> GetTicketTypes() => Ok(ticketTypes);
+
+        [HttpGet("{id}")]
+        public ActionResult<TicketType> GetTicketType(int id)
+        {
+            var ticketType = ticketTypes.FirstOrDefault(t => t.Id == id);
+            if (ticketType == null) return NotFound();
+            return Ok(ticketType);
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] TicketType ticketType)
+        {
+            ticketType.Id = ticketTypes.Max(t => t.Id) + 1;
+            ticketTypes.Add(ticketType);
+            return CreatedAtAction(nameof(GetTicketType), new { id = ticketType.Id }, ticketType);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] TicketType updatedTicketType)
+        {
+            var ticketType = ticketTypes.FirstOrDefault(t => t.Id == id);
+            if (ticketType == null) return NotFound();
+
+            ticketType.Name = updatedTicketType.Name;
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            var ticketType = ticketTypes.FirstOrDefault(t => t.Id == id);
+            if (ticketType == null) return NotFound();
+
+            ticketTypes.Remove(ticketType);
+            return NoContent();
+        }
+    }
+}
