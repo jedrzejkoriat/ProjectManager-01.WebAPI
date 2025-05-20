9ba4395d1af895a1b57d3ae22584b7c14918fa07
jedrzejkoriat <jedrzej.koriat@gmail.com>
2025-05-20 22:42:34 +0200

    feat: add RelationType enum, TicketRelation data model and controller

---
+++ Data/TicketRelation.cs
@@ -0,0 +1,10 @@
+﻿using ProjectManager_01.WebAPI.Enums;
+
+namespace ProjectManager_01.WebAPI.Data;
+
+public class TicketRelation
+{
+    public Guid TargetId { get; set; }
+    public Guid SourceId { get; set; }
+    public RelationType RelationType { get; set; }
+}
---
+++ Enums/RelationType.cs
@@ -0,0 +1,8 @@
+﻿namespace ProjectManager_01.WebAPI.Enums;
+
+public enum RelationType
+{
+    Blocks = 0,
+    Relates = 1,
+    Duplicates = 2
+}
