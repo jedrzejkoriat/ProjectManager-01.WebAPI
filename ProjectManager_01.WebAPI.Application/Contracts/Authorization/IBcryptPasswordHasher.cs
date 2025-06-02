0935b3e995c0b8e77113fcefd58e71ad13c85ee5
jedrzejkoriat <jedrzej.koriat@gmail.com>
2025-06-02 20:23:11 +0200

    chore: change Hasher to static class

--- ProjectManager_01.WebAPI.Application/Contracts/Authorization/IBcryptPasswordHasher.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ProjectManager_01.Application.Contracts.Authorization;
-
-public interface IBcryptPasswordHasher
-{
-    string HashPassword(string password);
-    bool VerifyPassword(string hashedPassword, string providedPassword);
-}
