015f4865e11884dafbda69e211d09ccf8be261e9
jedrzejkoriat <jedrzej.koriat@gmail.com>
2025-06-07 21:31:19 +0200

    feat: add exceptions and logging to permissions

--- ProjectManager_01.WebAPI.Application/Services/CommentService.cs
+++ ProjectManager_01.WebAPI.Application/Services/CommentService.cs
@@ -84,7 +84,7 @@ public class CommentService : ICommentService
 
     public async Task UpdateCommentAsync(CommentUpdateDto commentUpdateDto, Guid projectId)
     {
-        _logger.LogInformation("Update comment called. Comment: {CommentId}", commentUpdateDto.Id);
+        _logger.LogInformation("Updating comment called. Comment: {CommentId}", commentUpdateDto.Id);
 
         // Validate if project access is allowed
         await _projectAccessValidator.ValidateTicketProjectIdAsync(commentUpdateDto.TicketId, projectId);
@@ -94,23 +94,23 @@ public class CommentService : ICommentService
         // Check if operation is successful
         if (!await _commentRepository.UpdateAsync(comment))
         {
-            _logger.LogError("Udate comment failed. Comment: {CommentId}", commentUpdateDto.Id);
-            throw new OperationFailedException($"Update comment failed.");
+            _logger.LogError("Udating comment failed. Comment: {CommentId}", commentUpdateDto.Id);
+            throw new OperationFailedException($"Updating comment failed.");
         }
 
-        _logger.LogInformation("Update comment succesfull. Comment: {ComentId}", commentUpdateDto.Id);
+        _logger.LogInformation("Updating comment succesfull. Comment: {ComentId}", commentUpdateDto.Id);
     }
 
     public async Task DeleteCommentAsync(Guid commentId, Guid projectId)
     {
-        _logger.LogInformation("Delete comment called. Comment: {CommentId}", commentId);
+        _logger.LogInformation("Deleting comment called. Comment: {CommentId}", commentId);
 
         var comment = await _commentRepository.GetByIdAsync(commentId);
 
         // Check if comment exsists
         if (comment == null)
         {
-            _logger.LogError("Delete comment failed. Comment: {CommentId}", commentId);
+            _logger.LogError("Deleting comment failed. Comment: {CommentId}", commentId);
             throw new NotFoundException("Comment not found.");
         }
 
@@ -120,44 +120,44 @@ public class CommentService : ICommentService
         // Check if operation is successful
         if (!await _commentRepository.DeleteByIdAsync(commentId))
         {
-            _logger.LogError("Delete comment failed. Comment: {CommentId}", commentId);
-            throw new OperationFailedException("Delete comment failed.");
+            _logger.LogError("Deleting comment failed. Comment: {CommentId}", commentId);
+            throw new OperationFailedException("Deleting comment failed.");
         }
 
-        _logger.LogInformation("Delete comment succesfull. Comment: {CommentId}", commentId);
+        _logger.LogInformation("Deleting comment succesfull. Comment: {CommentId}", commentId);
     }
 
     public async Task DeleteByUserIdAsync(Guid userId, IDbTransaction transaction)
     {
-        _logger.LogInformation("Delete comments by userId called. User: {UserId}", userId);
+        _logger.LogInformation("Deleting comments by userId called. User: {UserId}", userId);
 
         // Check if operation is successful
         if (!await _commentRepository.DeleteAllByUserIdAsync(userId, transaction))
         {
-            _logger.LogError("Delete comments by userId failed. User: {UserId}", userId);
-            throw new OperationFailedException("Delete comments failed.");
+            _logger.LogError("Deleting comments by userId failed. User: {UserId}", userId);
+            throw new OperationFailedException("Deleting comments failed.");
         }
 
-        _logger.LogInformation("Delete comments by userId successfull. User: {UserId}", userId);
+        _logger.LogInformation("Deleting comments by userId successfull. User: {UserId}", userId);
     }
 
     public async Task DeleteByTicketIdAsync(Guid ticketId, IDbTransaction transaction)
     {
-        _logger.LogInformation("Delete comments by ticketId called. Ticket: {TicketId}", ticketId);
+        _logger.LogInformation("Deleting comments by ticketId called. Ticket: {TicketId}", ticketId);
 
         // Check if operation is successful
         if (!await _commentRepository.DeleteAllByTicketIdAsync(ticketId, transaction))
         {
-            _logger.LogError("Delete comments by ticketId failed. Ticket: {TicketId}", ticketId);
-            throw new OperationFailedException("Delete comments failed.");
+            _logger.LogError("Deleting comments by ticketId failed. Ticket: {TicketId}", ticketId);
+            throw new OperationFailedException("Deleting comments failed.");
         }
 
-        _logger.LogInformation("Delete comments by ticketId successfull. Ticket: {TicketId}", ticketId);
+        _logger.LogInformation("Deleting comments by ticketId successfull. Ticket: {TicketId}", ticketId);
     }
 
     public async Task<IEnumerable<CommentDto>> GetByTicketIdAsync(Guid ticketId)
     {
-        _logger.LogInformation("Get comments by ticketId called. Ticket: {TicketId}", ticketId);
+        _logger.LogInformation("Getting comments by ticketId called. Ticket: {TicketId}", ticketId);
 
         var comments = await _commentRepository.GetAllByTicketIdAsync(ticketId);
         _logger.LogInformation("Getting comments ({Count}) successfull.", comments.Count());
--- ProjectManager_01.WebAPI.Application/Services/PermissionService.cs
+++ ProjectManager_01.WebAPI.Application/Services/PermissionService.cs
@@ -1,8 +1,10 @@
 ﻿using System.Data;
 using AutoMapper;
+using Microsoft.Extensions.Logging;
 using ProjectManager_01.Application.Contracts.Repositories;
 using ProjectManager_01.Application.Contracts.Services;
 using ProjectManager_01.Application.DTOs.Permissions;
+using ProjectManager_01.Application.Exceptions;
 using ProjectManager_01.Application.Helpers;
 using ProjectManager_01.Domain.Models;
 
@@ -14,61 +16,107 @@ public class PermissionService : IPermissionService
     private readonly IMapper _mapper;
     private readonly IDbConnection _dbConnection;
     private readonly IProjectRolePermissionService _projectRolePermissionService;
+    private readonly ILogger<PermissionService> _logger;
 
     public PermissionService(
         IPermissionRepository permissionRepository,
         IMapper mapper,
         IDbConnection dbConnection,
-        IProjectRolePermissionService projectRolePermissionService)
+        IProjectRolePermissionService projectRolePermissionService,
+        ILogger<PermissionService> logger)
     {
         _permissionRepository = permissionRepository;
         _mapper = mapper;
         _dbConnection = dbConnection;
         _projectRolePermissionService = projectRolePermissionService;
+        _logger = logger;
     }
 
     public async Task CreatePermissionAsync(PermissionCreateDto permissionCreateDto)
     {
+        _logger.LogWarning("Creating permission called. Permission: {PermissionName}", permissionCreateDto.Name);
+
         var permission = _mapper.Map<Permission>(permissionCreateDto);
         permission.Id = Guid.NewGuid();
-        await _permissionRepository.CreateAsync(permission);
+
+        // Check if operation is successful
+        if (!await _permissionRepository.CreateAsync(permission))
+        {
+            _logger.LogError("Creating permission failed. Permission: {PermissionName}", permission.Name);
+            throw new Exception("Creating permission failed.");
+        }
+
+        _logger.LogInformation("Creating permission successful. Permission: {PermissionId}", permission.Id);
     }
 
     public async Task DeletePermissionAsync(Guid permissionId)
     {
+        _logger.LogWarning("Deleting permission transaction called. Permission: {PermissionId}", permissionId);
+
         using var transaction = DbTransactionHelper.BeginTransaction(_dbConnection);
 
         try
         {
-            await _permissionRepository.DeleteByIdAsync(permissionId, transaction);
+            // Check if operation is successful
+            if (!await _permissionRepository.DeleteByIdAsync(permissionId, transaction))
+            {
+                _logger.LogError("Deleting permission transaction failed. Permission: {PermissionId}", permissionId);
+                throw new OperationFailedException("Deleting permission transaction failed.");
+            }
+
             await _projectRolePermissionService.DeleteByPermissionIdAsync(permissionId, transaction);
 
             transaction.Commit();
+            _logger.LogInformation("Deleting permission transaction successful. Permission: {PermissionId}", permissionId);
         }
         catch
         {
             transaction.Rollback();
-            throw new Exception("Error while performing permission deletion transaction.");
+            _logger.LogError("Deleting permission transaction failed. Permission: {PermissionId}", permissionId);
+            throw;
         }
     }
 
     public async Task<IEnumerable<PermissionDto>> GetAllPermissionsAsync()
     {
+        _logger.LogInformation("Getting permissions called.");
+
         var permissions = await _permissionRepository.GetAllAsync();
+        _logger.LogInformation("Getting permissions ({Count}) succesfull.", permissions.Count());
 
         return _mapper.Map<IEnumerable<PermissionDto>>(permissions);
     }
 
     public async Task<PermissionDto> GetPermissionByIdAsync(Guid permissionId)
     {
+        _logger.LogInformation("Getting permission called. Permission: {PermissionId}", permissionId);
+
         var permission = await _permissionRepository.GetByIdAsync(permissionId);
 
+        // Check if operation is successful
+        if (permission == null)
+        {
+            _logger.LogError("Getting permission failed. Permission: {PermissionId}", permissionId);
+            throw new NotFoundException("Permission not found.");
+        }
+
+        _logger.LogInformation("Getting permission successful. Permission: {PermissionId}", permissionId);
         return _mapper.Map<PermissionDto>(permission);
     }
 
     public async Task UpdatePermissionAsync(PermissionUpdateDto permissionUpdateDto)
     {
+        _logger.LogInformation("Updating permission called. Permission: {PermissionId}", permissionUpdateDto.Id);
+
         var permission = _mapper.Map<Permission>(permissionUpdateDto);
-        await _permissionRepository.UpdateAsync(permission);
+
+        // Check if operation is successful
+        if (!await _permissionRepository.UpdateAsync(permission))
+        {
+            _logger.LogError("Updating permission failed. Permission: {PermissionId}", permission.Id);
+            throw new OperationFailedException("Updating permission failed.");
+        }
+
+        _logger.LogInformation("Updating permission successful. Permission: {PermissionId}", permission.Id);
     }
 }
